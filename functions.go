66edb1a63e77cabe474b55ea996e122a9fa0502a
Cioban George-Adrian <cgeorgeadrian99@gmail.com>
2024-12-19 15:03:02 +0200

    initial commit 2

--- functions.go
+++ functions.go
@@ -5,3 +5,6 @@ import "strings"
 func Replace(s string) string {
 	return strings.Replace(s, "Hello", "Goodbye", -1)
 }
+func Replace2(s string) string {
+	return strings.Replace(s, "Hello", "Goodbye", -1)
+}
